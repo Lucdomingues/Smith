0b571c7759f6ec82639c6f8668ae8b043d33f7de
Lucdomingues <lojalucc@outlook.com>
2023-03-15 13:07:12 -0300

    [feat]validation-users--file:middlewares-validateUsers

--- src/routers/users.route.ts
+++ src/routers/users.route.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import usersController from '../controllers/users.controller';
+import {
+  validateUsername, validateVocation, validateLevel, validatePassword,
+} from '../middlewares/validateUsers';
 
 const route = express.Router();
 
-route.post('/', usersController.createUser);
+route.post(
+  '/',
+  validateUsername,
+  validateVocation,
+  validateLevel,
+  validatePassword,
+  usersController.createUser,
+);
 
 export default route;
