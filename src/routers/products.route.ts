deaacc139542e62e25402bfd7a6728f423363e2c
Lucdomingues <lojalucc@outlook.com>
2023-03-15 12:49:12 -0300

    [feat]validation-products---files:minddlewares

--- src/routers/products.route.ts
+++ src/routers/products.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import productsController from '../controllers/products.controller';
+import { validateName, validateAmount } from '../middlewares/validateProducts';
 
 const route = express.Router();
 
 route.get('/', productsController.getAllProducts);
-route.post('/', productsController.createProdutc);
+route.post('/', validateName, validateAmount, productsController.createProdutc);
 
 export default route;
